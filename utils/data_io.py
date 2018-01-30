f1f1244eeeaf672faf923aa45297c9fe736064cc
marrabld <marrabld@gmail.com>
2018-01-30 17:02:05 +0800

    Added fill_na_nn

--- utils/data_io.py
+++ utils/data_io.py
@@ -1,9 +1,11 @@
 import csv
+import numpy as np
+
 
 def csv_to_dict(input_file):
     reader = csv.DictReader(open(input_file, 'r'))
     data = dict.fromkeys(reader.fieldnames)
-    
+
     for i_iter, row in enumerate(reader):
         for key in data.keys():
             if i_iter <= 0:
@@ -11,3 +13,18 @@ def csv_to_dict(input_file):
             else:
                 data[key].append(row[key])
     return data
+
+
+def fill_na_nn(df, val=np.nan):
+    """
+    replace nans by using nearest neighbour
+
+    :param df: dataframe
+    :param val:
+    :return:
+    """
+
+    df.where(df.replace(to_replace=val, value=np.nan),
+             other=(df.fillna(method='ffill') + df.fillna(method='bfill')) / 2)
+
+    return df
